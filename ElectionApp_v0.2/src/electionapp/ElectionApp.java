d5876b78f6f5a979777e8b61ce6b84bb109cf0df
cianharry <35872966+cianharry@users.noreply.github.com>
2018-11-27 19:54:19 +0000

    Add files via upload

---
+++ ElectionApp_v0.2/src/electionapp/ElectionApp.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package electionapp;
+
+/**
+ *
+ * @author cianh
+ */
+public class ElectionApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        MainGUI myGUI=new MainGUI();
+        myGUI.setVisible(true);
+        
+    }
+    
+}
